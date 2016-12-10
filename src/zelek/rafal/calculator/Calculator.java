11e8825fc8a53ab515f0762e46a55d02e1b287fb
rafal <roen00@gmail.com>
2016-12-10 17:43:13 +0100

    added basic template for Calculator and CalculatorTest

---
+++ src/zelek/rafal/calculator/Calculator.java
@@ -0,0 +1,5 @@
+package zelek.rafal.calculator;
+
+public class Calculator {
+
+}
---
+++ test/zelek/rafal/calculator/CalculatorTest.java
@@ -0,0 +1,14 @@
+package zelek.rafal.calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
