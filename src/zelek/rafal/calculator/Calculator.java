5ce37173ceaf0ca0c5eddf7b843f89a6b4da1b1e
rafal <roen00@gmail.com>
2016-12-10 17:54:35 +0100

    added implemented 'add' functionality with tests

--- src/zelek/rafal/calculator/Calculator.java
+++ src/zelek/rafal/calculator/Calculator.java
@@ -2,4 +2,8 @@ package zelek.rafal.calculator;
 
 public class Calculator {
 
+	public int add(int i, int j) {
+		return i + j;
+	}
+
 }
--- test/zelek/rafal/calculator/CalculatorTest.java
+++ test/zelek/rafal/calculator/CalculatorTest.java
@@ -7,8 +7,24 @@ import org.junit.Test;
 public class CalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testThat1Plus3Is4() {
+		Calculator sut = new Calculator();
+		int result = sut.add(1,3);
+		assertSame(result, 4);
 	}
 
+	@Test
+	public void testThat0Plus0Is0() {
+		Calculator sut = new Calculator();
+		int result = sut.add(0,0);
+		assertSame(result, 0);
+	}
+
+	@Test
+	public void testThatNegative10Plus10Is0() {
+		Calculator sut = new Calculator();
+		int result = sut.add(-10,10);
+		assertSame(result, 0);
+	}
+	
 }
